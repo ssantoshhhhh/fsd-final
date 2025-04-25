f5c2f3c6a25d1e9380939fc4a1f320f09cf5e432
Santosh Seelaboina <163726808+ssantoshhhhh@users.noreply.github.com>
2025-04-25 23:51:25 +0530

    Update contact.php
    removed original SMTP

--- contact.php
+++ contact.php
@@ -38,8 +38,8 @@ if (isset($_POST['name']) &&
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                 // Enable SMTP authentication
-        $mail->Username   = 'santoshwebtechnology@gmail.com';     // SMTP username
-        $mail->Password   = 'rrrwivywutbdwxul';                  // Use app password, not actual password
+        $mail->Username   = '';     // SMTP username
+        $mail->Password   = '';     // Use app password, not actual password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable implicit TLS encryption
         $mail->Port       = 465;                                  // TCP port to connect to
 
